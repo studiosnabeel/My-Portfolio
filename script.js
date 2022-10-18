65d160371b979b9a942ff892e38519e4f05a9672
Serg Skol <sps676@gmail.com>
2022-10-18 09:49:29 +0200

    Change let to const in js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let burgerMenu = document.querySelector('#burgerMenu');
-let crossClose = document.querySelector('#crossClose');
-let menuBar = document.querySelector('#menuBar');
-let menuLinks = document.querySelectorAll('.menuLinks');
+const burgerMenu = document.querySelector('#burgerMenu');
+const crossClose = document.querySelector('#crossClose');
+const menuBar = document.querySelector('#menuBar');
+const menuLinks = document.querySelectorAll('.menuLinks');
 
 burgerMenu.addEventListener('click', () => {
   menuBar.classList.toggle('open-bar');
