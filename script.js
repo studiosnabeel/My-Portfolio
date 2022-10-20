b72db260e3854f6ef5b1849bca98f77ef2d48954
Nabeel Akbar <studiosnabeel@gmail.com>
2022-10-21 02:07:59 +0500

    Add Event listener to form

--- script.js
+++ script.js
@@ -148,3 +148,7 @@ popContainer.forEach((close) => {
 const form = document.querySelector('.form-js');
 const email = document.querySelector('#email-js');
 const error = document.querySelector('.error-js');
+
+form.addEventListener('submit', (action) => {
+  action.preventDefault();
+  const mail = email.value;
